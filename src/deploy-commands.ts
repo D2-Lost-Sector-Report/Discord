5c079e680e257ea4b3f1dabeae79240d71b1f46f
Moons <me@moons.bio>
2025-05-28 10:28:13 +0200

    refactor code style and formatting

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,18 +1,15 @@
-import {REST, Routes} from 'discord.js'
-import {config} from './config'
-import {commands} from './commands'
+import { REST, Routes } from "discord.js";
+import { config } from "./config";
+import { commands } from "./commands";
 
-const commandsData = Object.values(commands).map((command) => command.data)
+const commandsData = Object.values(commands).map((command) => command.data);
 
-const rest = new REST().setToken(config.DISCORD_TOKEN)
+const rest = new REST().setToken(config.DISCORD_TOKEN);
 
 export async function deployCommands() {
-  console.log('Started refreshing application (/) commands.')
+  console.log("Started refreshing application (/) commands.");
 
-  await rest.put(
-    Routes.applicationCommands(config.DISCORD_CLIENT_ID),
-    {
-      body: commandsData,
-    }
-  )
+  await rest.put(Routes.applicationCommands(config.DISCORD_CLIENT_ID), {
+    body: commandsData,
+  });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,39 @@
-import {Client} from 'discord.js'
-import {config} from './config'
-import {commands} from './commands'
-import {deployCommands} from './deploy-commands'
+import { Client } from "discord.js";
+import { config } from "./config";
+import { commands } from "./commands";
+import { deployCommands } from "./deploy-commands";
 
 const client = new Client({
-  intents: ['Guilds', 'GuildMessages', 'DirectMessages'],
-})
+  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+});
 
-client.once('ready', () => {
-  console.log('Bot is ready!')
+client.once("ready", () => {
+  console.log("Bot is ready!");
 
   deployCommands()
     .then(() => {
-      console.log('Registered commands successfully!')
+      console.log("Registered commands successfully!");
     })
     .catch((error) => {
-      console.error('Failed to register commands:', error)
-    })
-})
+      console.error("Failed to register commands:", error);
+    });
+});
 
-client.on('interactionCreate', async (interaction) => {
+client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) {
-    return
+    return;
   }
-  const {commandName} = interaction
+  const { commandName } = interaction;
   if (commands[commandName as keyof typeof commands]) {
-    await commands[commandName as keyof typeof commands].execute(interaction)
+    await commands[commandName as keyof typeof commands].execute(interaction);
   }
-})
+});
 
-client.login(config.DISCORD_TOKEN)
+client
+  .login(config.DISCORD_TOKEN)
   .then(() => {
-    console.log('Logged in successfully!')
+    console.log("Logged in successfully!");
   })
   .catch((error) => {
-    console.error('Failed to log in:', error)
-  })
+    console.error("Failed to log in:", error);
+  });
