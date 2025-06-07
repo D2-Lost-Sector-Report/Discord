10004ac6127e8f55f36dc596afd528279003c89e
Moons <me@moons.bio>
2025-06-07 23:50:00 +0200

    Update clientReady event to ensure rewards list is populated

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -13,8 +13,8 @@ const handler: EventHandler<"ready"> = {
     if (guild) {
       await guild.emojis.fetch();
       await writeEmoteCacheToFile(guild.emojis.cache);
-      await populateRewardsList();
     }
+    await populateRewardsList();
   },
 };
 
