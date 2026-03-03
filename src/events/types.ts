5daf5c434a5a28f264dfff8f0884b7a22da75c2d
Barry Briggs <quifflet@gmail.com>
2026-03-03 09:15:56 +0000

    Fix console error on server leave

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,6 +1,7 @@
 import type { ClientEvents } from "discord.js";
 
 export interface EventHandler<K extends keyof ClientEvents = keyof ClientEvents> {
+  guilds: any;
   name: K;
   once?: boolean;
   execute: (...args: any[]) => any;
