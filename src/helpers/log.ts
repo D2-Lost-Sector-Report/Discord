5eb40ecccd9a1fb25ea81f79c11f6ff4acebeca3
Barry Briggs <quifflet@gmail.com>
2026-03-11 23:46:34 +0000

    Fix error on leaving a server

--- src/helpers/log.ts
+++ src/helpers/log.ts
@@ -21,12 +21,17 @@ export async function sendToLoggingChannel(
 
   // If not found, use broadcastEval to find the right shard
   if (client.shard) {
+    // avoid async/await in the eval callback so tsup doesn't inject
+    // a helper that gets minified to an undefined identifier
     await client.shard.broadcastEval(
-      async (c, { channelId, content }) => {
-        const channel = await c.channels.fetch(channelId).catch(() => null);
-        if (channel && channel.isTextBased() && 'send' in channel) {
-          await channel.send(content);
-        }
+      (c, { channelId, content }) => {
+        return c.channels.fetch(channelId)
+          .catch(() => null)
+          .then(channel => {
+            if (channel && channel.isTextBased() && 'send' in channel) {
+              channel.send(content);
+            }
+          });
       },
       {
         context: { channelId, content }
